ad9d77c521e83417dbbaa268bdffa34f9aecfc9a
Graham Stark <graham.stark@virtual-worlds.biz>
2025-10-01 19:42:37 +0100

    Start of Oxygen implementation.

--- src/MicrosimAPI.jl
+++ src/MicrosimAPI.jl
@@ -3,8 +3,48 @@ module MicrosimAPI
 using JSON3
 using Oxygen
 using HTTP
+using Random
+using Parameters
+using Dates
+
+const SESSION_TIMEOUT = Second(24 * 60 * 60) # 24 Hours
+
+mutable struct Session
+    created :: Date
+    expires :: Date
+    last_activity :: Date
+end
+
+sessions = Dict{String,Session}()
+
+# Generate secure session token
+function generateSessionToken()
+  return randstring('0':'9', 32)
+end
+
+
+@get "/session/start" function(req::HTTP.Request)
+    JsonFragment
+    session_token = generateSessionToken()
+    session = Session( 
+        now(),
+        now() + SESSION_TIMEOUT,
+        now()
+    )
+    sessions[session_token] = session
+    return json( (; 
+        session_token, 
+        success = true, 
+        expires_in = SESSION_TIMEOUT ))
+end
+
+@get "/session/destroy" function(req::HTTP.Request)
+
+end
+
 
 include("scotben.jl")
-# Write your package code here.
+
+Oxygen.serve()
 
 end
--- src/scotben.jl
+++ src/scotben.jl
@@ -1,30 +1,59 @@
-#=
-@get "/scotben/session/start" function(req::HTTP.Request)
+"""
 
+"""
+
+@with_kw mutable struct SBRunData
+    owner::String
+    thing::String
 end
 
-@get "/scotben/session/destroy" function(req::HTTP.Request)
+SB_RUNS = Dict{String,RunData}()
 
-end
+"""
 
+"""
 @get "/scotben/params/list-available" function(req::HTTP.Request)
 
 end
+
+"""
+
+"""
 @get "/scotben/params/initialise/[defaults]" function(req::HTTP.Request)
 
 end
+
+"""
+
+"""
 @get "/scotben/params/set" function(req::HTTP.Request)
 
 end
+
+"""
+
+"""
 @get "/scotben/params/validate" function(req::HTTP.Request)
 
 end
+
+"""
+
+"""
 @get "/scotben/params/describe" function(req::HTTP.Request)
 
 end
+
+"""
+
+"""
 @get "/scotben/params/subsys" function(req::HTTP.Request)
 
 end
+
+"""
+
+"""
 @get "/scotben/params/helppage" function(req::HTTP.Request)
 
 end
@@ -32,83 +61,79 @@ end
 
 end
 
+#=
 or /params@get "/scotben/[subsys]/set ... for individual pages" function(req::HTTP.Request)
-
-end
-
-@get "/scotben/settings/set" function(req::HTTP.Request)
-
-end
-
-@get "/scotben/settings/validate" function(req::HTTP.Request)
-
-end
-
-...
-
-@get "/scotben/run/submit" function(req::HTTP.Request)
-
-end
-@get "/scotben/run/status" function(req::HTTP.Request)
-
-end
-@get "/scotben/run/abort" function(req::HTTP.Request)
-
-end
-
-@get "/scotben/output/items" function(req::HTTP.Request)
-
-end
-@get "/scotben/output/phunpak" function(req::HTTP.Request)
-
-end
-@get "/scotben/output/labels" function(req::HTTP.Request)
-
-end
-@get "/scotben/output/fetch/item" function(req::HTTP.Request)
 
 end
 =#
 
-@get "/scotben/session/start" function(req::HTTP.Request)
-    
+
+"""
+
+"""
+@get "/scotben/settings/set" function(req::HTTP.Request)
 
 end
 
-@get "/scotben/session/destroy" function(req::HTTP.Request)
+
+"""
+
+"""
+@get "/scotben/settings/validate" function(req::HTTP.Request)
 
 end
 
 
-@get "/scotben/params/list-available" function(req::HTTP.Request)
+"""
+
+"""
+@get "/scotben/run/submit" function(req::HTTP.Request)
 
 end
 
-@get "/scotben/params/initialise/[defaults]" function(req::HTTP.Request)
+"""
+
+"""
+@get "/scotben/run/status" function(req::HTTP.Request)
 
 end
 
-@get "/scotben/params/set" function(req::HTTP.Request)
+
+"""
+
+"""
+@get "/scotben/run/abort" function(req::HTTP.Request)
 
 end
 
-@get "/scotben/params/validate" function(req::HTTP.Request)
+
+"""
+
+"""
+@get "/scotben/output/items" function(req::HTTP.Request)
 
 end
 
-@get "/scotben/params/describe" function(req::HTTP.Request)
+
+"""
+
+"""
+@get "/scotben/output/phunpak" function(req::HTTP.Request)
 
 end
 
-@get "/scotben/params/subsys" function(req::HTTP.Request)
+
+"""
+
+"""
+@get "/scotben/output/labels" function(req::HTTP.Request)
 
 end
 
-@get "/scotben/params/helppage" function(req::HTTP.Request)
+
+"""
+
+"""
+@get "/scotben/output/fetch/item" function(req::HTTP.Request)
 
 end
-
-@get "/scotben/params/labels" function(req::HTTP.Request)
-
-end
-
