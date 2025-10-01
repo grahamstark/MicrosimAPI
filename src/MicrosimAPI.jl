59c0408cef5b42a28f3877a32775e418180893d1
Graham Stark <graham.stark@virtual-worlds.biz>
2025-10-01 13:41:47 +0100

    Working on json mappings.

--- src/MicrosimAPI.jl
+++ src/MicrosimAPI.jl
@@ -2,8 +2,9 @@ module MicrosimAPI
 
 using JSON3
 using Oxygen
+using HTTP
 
-
+include("scotben.jl")
 # Write your package code here.
 
 end
---
+++ src/scotben.jl
@@ -0,0 +1,114 @@
+#=
+@get "/scotben/session/start" function(req::HTTP.Request)
+
+end
+
+@get "/scotben/session/destroy" function(req::HTTP.Request)
+
+end
+
+@get "/scotben/params/list-available" function(req::HTTP.Request)
+
+end
+@get "/scotben/params/initialise/[defaults]" function(req::HTTP.Request)
+
+end
+@get "/scotben/params/set" function(req::HTTP.Request)
+
+end
+@get "/scotben/params/validate" function(req::HTTP.Request)
+
+end
+@get "/scotben/params/describe" function(req::HTTP.Request)
+
+end
+@get "/scotben/params/subsys" function(req::HTTP.Request)
+
+end
+@get "/scotben/params/helppage" function(req::HTTP.Request)
+
+end
+@get "/scotben/params/labels" function(req::HTTP.Request)
+
+end
+
+or /params@get "/scotben/[subsys]/set ... for individual pages" function(req::HTTP.Request)
+
+end
+
+@get "/scotben/settings/set" function(req::HTTP.Request)
+
+end
+
+@get "/scotben/settings/validate" function(req::HTTP.Request)
+
+end
+
+...
+
+@get "/scotben/run/submit" function(req::HTTP.Request)
+
+end
+@get "/scotben/run/status" function(req::HTTP.Request)
+
+end
+@get "/scotben/run/abort" function(req::HTTP.Request)
+
+end
+
+@get "/scotben/output/items" function(req::HTTP.Request)
+
+end
+@get "/scotben/output/phunpak" function(req::HTTP.Request)
+
+end
+@get "/scotben/output/labels" function(req::HTTP.Request)
+
+end
+@get "/scotben/output/fetch/item" function(req::HTTP.Request)
+
+end
+=#
+
+@get "/scotben/session/start" function(req::HTTP.Request)
+    
+
+end
+
+@get "/scotben/session/destroy" function(req::HTTP.Request)
+
+end
+
+
+@get "/scotben/params/list-available" function(req::HTTP.Request)
+
+end
+
+@get "/scotben/params/initialise/[defaults]" function(req::HTTP.Request)
+
+end
+
+@get "/scotben/params/set" function(req::HTTP.Request)
+
+end
+
+@get "/scotben/params/validate" function(req::HTTP.Request)
+
+end
+
+@get "/scotben/params/describe" function(req::HTTP.Request)
+
+end
+
+@get "/scotben/params/subsys" function(req::HTTP.Request)
+
+end
+
+@get "/scotben/params/helppage" function(req::HTTP.Request)
+
+end
+
+@get "/scotben/params/labels" function(req::HTTP.Request)
+
+end
+
